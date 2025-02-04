ce7598b8da49ce72e90359f61ef1ee1c9df0bcbc
William Owen <william.owen.bsc@gmail.com>
2025-02-04 13:17:25 -0800

    Create Projects Page

---
+++ cptc-CPW219-eCommerceSite/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cptc_CPW219_eCommerceSite.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
