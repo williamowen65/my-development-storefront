22f764b4ab878a1f78f4e8ba656531fd4a719b13
William Owen <william.owen.bsc@gmail.com>
2025-02-05 06:59:53 -0800

    _partials WIP

---
+++ cptc-CPW219-eCommerceSite/Views/Home/_partials/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cptc_CPW219_eCommerceSite.Views.Home._partials
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
