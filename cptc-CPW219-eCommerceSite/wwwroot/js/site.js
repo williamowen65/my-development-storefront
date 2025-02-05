403e7550c5c1048e28bdd036f6d572a5d1779aef
William Owen <william.owen.bsc@gmail.com>
2025-02-04 20:34:39 -0800

    Update README, enhance pricing model form, and improve testimonials

--- cptc-CPW219-eCommerceSite/wwwroot/js/site.js
+++ cptc-CPW219-eCommerceSite/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    Array.from(document.querySelectorAll('#pricingModelForm input'))
+        .forEach(e => {
+
+            e.addEventListener('change', () => {
+
+                // enable button
+                document.querySelector("#selectPriceModel").classList.remove("disabled")
+            })
+        })
+})
