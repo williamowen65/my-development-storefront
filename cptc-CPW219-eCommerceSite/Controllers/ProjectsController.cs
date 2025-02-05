c4a7cea39ff25a1d2406399aaff1b5a77edc019a
William Owen <william.owen.bsc@gmail.com>
2025-02-05 12:34:34 -0800

    Updates on projects page

--- cptc-CPW219-eCommerceSite/Controllers/ProjectsController.cs
+++ cptc-CPW219-eCommerceSite/Controllers/ProjectsController.cs
@@ -8,5 +8,13 @@ namespace cptc_CPW219_eCommerceSite.Controllers
         {
             return View();
         }
+
+        [Route("projects/{id}")]
+        public IActionResult Projects(string id)
+        {
+            // You can use the id parameter to fetch the specific project details
+            ViewData["ProjectId"] = id;
+            return View("_projects/" + id);
+        }
     }
 }
