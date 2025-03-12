90564e98285a580da6e3645474ada01c6adf0f1e
William Owen <william.owen.bsc@gmail.com>
2025-03-12 09:22:33 -0700

    Working Registration page

---
+++ cptc-CPW219-eCommerceSite/Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cptc_CPW219_eCommerceSite.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string? Phone { get; set; }
+
+        public string? Username { get; set; }
+
+    }
+
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[!@#$%^&*(),.?\"":{}|<>])(?=.*[a-z])(?=.*[A-Z]).+$", ErrorMessage = "The Password must contain at least one special character, one uppercase letter, and one lowercase letter.")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
