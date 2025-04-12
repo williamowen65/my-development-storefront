39ea8497910b1c3b57ade1baeb818294b169e1e0
William Owen <william.owen.bsc@gmail.com>
2025-04-12 11:59:00 -0700

    Form UX

--- my-development-storefront/wwwroot/js/modal-crud.js
+++ my-development-storefront/wwwroot/js/modal-crud.js
@@ -1,13 +1,24 @@
 ﻿$(document).ready(function () {
-    $('*[open-sesame]').click(function (e) {
-        const target = e.target.getAttribute('open-sesame')
+    // Use proper event delegation with on() method
+    $(document).on('click', '*[open-sesame]', function (e) {
+        const target = e.currentTarget.getAttribute('open-sesame');
         $.get(target, function (data) {
             const modal = $("#modal-1");
             const modalContent = modal.find('#modal-1-content');
             if (modalContent.length) {
                 modalContent.html(data); // Set the child content
-                // Use Bootstrap's shown.bs.modal event instead of setTimeout
+
+                // Use Bootstrap's shown.bs.modal event
                 modal.one('shown.bs.modal', function () {
+                    setForm()
+                });
+
+                if (modal.hasClass('show')) {
+                    setForm()
+                }
+
+
+                function setForm() {
                     const inputs = modal.find('input, textarea, select');
                     const firstInput = inputs.get(0);
                     // focus the first input
@@ -15,11 +26,16 @@
                         firstInput.focus();
                     }
 
-                    setFormSubmissionListener(modal)
+                    setFormSubmissionListener(modal);
+                }
+
+                // Add event handler to clean up when the modal is hidden
+                modal.one('hidden.bs.modal', function () {
+                    // Optional: Clear modal content when closed
+                    // modalContent.html('');
                 });
 
                 modal.modal('show'); // Show the modal
-              
             } else {
                 console.error("Modal content element not found!");
             }
@@ -30,53 +46,46 @@
     });
 
     function setFormSubmissionListener(modal) {
-
-        const form = modal.find('form').get(0)
-        console.log({form})
+        const form = modal.find('form').get(0);
+        console.log({ form });
         if (form) {
-            // Set lisetner
-            $(form).on('submit', function(e) {
-
+            // Remove any existing handlers to prevent duplicates
+            $(form).off('submit').on('submit', function (e) {
                 e.preventDefault();
 
-                var form = $(this)
-                const data = form.serialize()
-                console.log({ form, data })
+                var form = $(this);
+                const data = form.serialize();
+                console.log({ form, data });
+
                 $.ajax({
                     type: form.attr('method'),
                     url: form.attr('action'),
                     data: form.serialize(),
                     success: function (res) {
                         if (res.success) {
-
-                            alert("successs")
+                            alert("successs");
                             modal.modal('hide');
                         } else {
                             modal.find('.modal-content').html(res);
                         }
                     }
                 });
-
-             
-            })
-
+            });
         }
     }
-
-
-    //const modals = document.querySelectorAll('.modal');
-    //Array.from(modals).forEach(modal => {
-    //    // Check if the modal has inputs
-    //    modal.addEventListener("hide.bs.modal", (e) => {
-    //        const hasInputs = modal.querySelectorAll('input, textarea, select').length > 0;
-    //        if (hasInputs) {
-    //            const userConfirmed = confirm("Do you wish to close the contact form?");
-    //            if (!userConfirmed) {
-    //                e.preventDefault(); // Prevent the modal from closing
-    //            }
-    //        }
-    //    })
-    //})
- 
-      
 });
+
+
+//const modals = document.querySelectorAll('.modal');
+//Array.from(modals).forEach(modal => {
+//    // Check if the modal has inputs
+//    modal.addEventListener("hide.bs.modal", (e) => {
+//        const hasInputs = modal.querySelectorAll('input, textarea, select').length > 0;
+//        if (hasInputs) {
+//            const userConfirmed = confirm("Do you wish to close the contact form?");
+//            if (!userConfirmed) {
+//                e.preventDefault(); // Prevent the modal from closing
+//            }
+//        }
+//    })
+//})
